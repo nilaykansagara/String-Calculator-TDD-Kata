e368fa24ae678da60b81d73b801ec200ae3f0f9d
Nilay Kansagara <nilaykansagara@gmail.com>
2024-07-30 00:44:14 +0530

    Handle different delimiter

--- StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator.cs
@@ -10,8 +10,18 @@
                 return 0;
             }
 
+            // list of the delimiters
+            var delimiters = new List<char> { ',', '\n' };
+
+            // check for different delimiter
+            if (numbers.StartsWith("//"))
+            {
+                delimiters.Add(numbers[2]);
+                numbers = numbers.Substring(4);
+            }
+
             // convert all numbers to int and sum up them
-            return numbers.Split(',', '\n')
+            return numbers.Split(delimiters.ToArray())
                     .Select(x => Convert.ToInt32(x))
                     .Sum();
         }
