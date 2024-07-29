52339086e3c3c2aea2ca79be0c065f75fe5200c4
Nilay Kansagara <nilaykansagara@gmail.com>
2024-07-29 23:35:43 +0530

    Add test for empty string

---
+++ StringCalculator.UnitTests/AddTests.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+using FluentAssertions;
+
+namespace StringCalculator.UnitTests
+{
+    public class AddTests
+    {
+        private readonly StringCalculator _stringCalculator;
+
+        public AddTests()
+        {
+            _stringCalculator = new StringCalculator();
+        }
+
+        [Fact]
+        public void Add_EmptyString_ReturnsZero()
+        {
+            // Act
+            var result = _stringCalculator.Add("");
+
+            // Assert
+            result.Should().Be(0);
+        }
+    }
+}
