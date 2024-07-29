b800990d901a8bb5ba8d6938c14c4f2932cfa46e
Nilay Kansagara <nilaykansagara@gmail.com>
2024-07-30 03:29:56 +0530

    Refactor the Add method

--- StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator.cs
@@ -5,12 +5,27 @@
         public int Add(string numbers)
         {
             // check null or empty string
-            if (String.IsNullOrEmpty(numbers))
+            if (string.IsNullOrEmpty(numbers))
             {
                 return 0;
             }
 
-            // list of the delimiters
+            // get delimiters
+            var delimiters = ExtractDelimiters(ref numbers);
+
+            // convert all numbers from string to integers
+            var values = numbers.Split(delimiters.ToArray(), StringSplitOptions.None)
+                .Select(x => Convert.ToInt32(x));
+
+            // check for negative numbers
+            CheckNegativeNumbers(values.ToList());
+
+            // sum up all numbers less than or equals to 1000
+            return values.Where(x => x <= 1000).Sum();
+        }
+
+        private static List<string> ExtractDelimiters(ref string numbers)
+        {
             var delimiters = new List<string> { ",", "\n" };
 
             // check for different delimiter
@@ -19,34 +34,24 @@
                 int newLineDelimiterIndex = numbers.IndexOf('\n');
                 var delimiterString = numbers.Substring(2, newLineDelimiterIndex - 2);
 
-                // check if delimiter is longer than one character
-                if (delimiterString.Length > 1)
-                {
-                    // split delimiter string to remove brackets
-                    var differentDelimiters = delimiterString.Split(new[] { "[", "]" }, StringSplitOptions.RemoveEmptyEntries);
-                    delimiters.AddRange(differentDelimiters.ToList());
-                }
-                else
-                {
-                    delimiters.Add(numbers[2].ToString());
-                }
+                // if delimiter string is longer than one character, split it to remove brackets
+                delimiters.AddRange(delimiterString.Length > 1
+                    ? delimiterString.Split(new[] { "[", "]" }, StringSplitOptions.RemoveEmptyEntries)
+                    : new[] { delimiterString });
+
                 numbers = numbers.Substring(newLineDelimiterIndex + 1);
             }
 
-            // convert all numbers from string to integers
-            var values = numbers.Split(delimiters.ToArray(), StringSplitOptions.None)
-                .Select(x => Convert.ToInt32(x))
-                .Where(x => x <= 1000);
+            return delimiters;
+        }
 
-            // check for negative number
-            var negativeNumbers = values.Where(x => x < 0).ToList();
+        private static void CheckNegativeNumbers(List<int> numbers)
+        {
+            var negativeNumbers = numbers.Where(x => x < 0).ToList();
             if (negativeNumbers.Any())
             {
                 throw new Exception($"Negative numbers not allowed: {string.Join(",", negativeNumbers)}");
             }
-
-            // sum up all numbers
-            return values.Sum();
         }
     }
 }
